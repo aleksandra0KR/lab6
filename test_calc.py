c4647121dc21a1c9f053621bb5a2d2aa4253c0cb
aleksandra0KR <114208185+aleksandra0KR@users.noreply.github.com>
2022-12-14 10:16:07 +0300

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -9,7 +9,7 @@ def test_answer():
     assert calculate(3,4,'-') == -1
 
 def test_answer():
-    assert calculate(3,4,'*') == 19
+    assert calculate(3,4,'*') == 16
 
 def test_answer():
     assert calculate(3,3,'/') == 1
