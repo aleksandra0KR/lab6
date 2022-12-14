d46d5a1c23f404cc26e6f5458d1b78f0693d23ff
aleksandra0KR <114208185+aleksandra0KR@users.noreply.github.com>
2022-12-14 06:13:24 +0300

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,7 +1,15 @@
 import calc
-print('File imported!')
-assert calc.simple_calculator("2+2") == 4, "correct addition: should be 4"
-assert calc.simple_calculator("2-2") == 0, "correct subtraction: should be 0"
-assert calc.simple_calculator("2/2") == 1, "correct division: should be 1"
-assert calc.simple_calculator("2*2") == 4, "correct multiplication: should be 4"
-print('all tests passed!')
+def test_answer():
+    assert calculate(3) == 5
+
+def test_answer():
+    assert calculate(3,4,'+') == 7
+
+def test_answer():
+    assert calculate(3,4,'-') == -1
+
+def test_answer():
+    assert calculate(3,4,'*') == 12
+
+def test_answer():
+    assert calculate(3,3,'/') == 1
