36197e9ffb620af0e29c422082efc25a72ad0396
aleksandra0KR <114208185+aleksandra0KR@users.noreply.github.com>
2022-12-14 10:56:43 +0300

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,11 +1,20 @@
 import calc
 
-def test_answer():
-    assert calculate("3 + 4") == 7
 
-def test_answer():
-    assert calculate("3 - 4") == -1
+def test_plus1():
+    output = calc.calc("3 + 4")
+    assert output == 7
 
-def test_answer():
-    assert calculate("3 * 4") == 120
 
+def test_plus2():
+    output = calc.calc("3 - 4")
+    assert output == -1
+
+
+def test_plus3():
+    output = calc.calc("3 * 4")
+    assert output == 12
+    
+test_plus1()
+test_plus2()
+test_plus3()
